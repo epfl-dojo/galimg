769cd90ddf86d4784a937d848a9089205eb9fedc
ponsfrilus <ponsfrilus@gmail.com>
2015-12-04 12:04:55 +0100

    Dojo du 2015-11-04

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # galimg
 Create a image gallery in BASH
+
+Usage: ./galimg.sh inputfolder outputfolder
+
+# ToDo
+1) get all image
+2) resize image in ouputfolder/thumbnails
+3) copy original images in ouputfolder/original
+4) create an index.html file with gallery
+
--- galimg.sh
+++ galimg.sh
@@ -1,2 +1,27 @@
 #!/bin/bash
+set -e -x
+src=$1
+dest=$2
 
+mkthumbnails() {
+  mkdir -p $dest/thumbnails
+  for i in $(find $src -name '*.jpeg'); 
+	  do 
+		  echo "Processing $i"; 
+		  thumbFile=$(echo $i| cut -d \/ -f 2)
+	      convert $i -resize '200x100' $dest/thumbnails/$thumbFile
+  done
+}
+
+copy_originals() {
+  mkdir $dest/original || true
+  cp -r $src/. $dest/original
+}
+
+make_index() {
+  (cd $src; ls -1) | perl -ne 'chomp; print qq(<a href="original/$_"><img src="thumbnails/$_"></a>)' > $dest/index.html
+}
+
+# mkthumbnails
+# copy_originals
+make_index
